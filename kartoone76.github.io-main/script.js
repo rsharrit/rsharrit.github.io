ce5ce0ac9b7bf084c9108968a5b5ef35522d51cb
rsharrit <rsharrit@samford.edu>
2024-10-23 13:52:20 -0500

    Delete kartoone76.github.io-main/script.js

--- kartoone76.github.io-main/script.js
+++
@@ -1,7 +0,0 @@
-function downloadData() {
-  event.preventDefault();
-  var startdate = document.getElementById("startdate").value;
-  var enddate = document.getElementById("enddate").value;
-  alert(startdate);
-  alert(enddate);
-}
